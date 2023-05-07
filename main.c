617d0d76f46f4e1493cf5260b6b75cd58af43fa0
Quiann <quiann.zolfaghari@ucr.ac.cr>
2023-05-06 22:27:31 -0600

    added the free matrix memory method and it reads the input now

--- main.c
+++ main.c
@@ -7,8 +7,16 @@
 
 int main(void) {
     int rows = 0, cols = 0;
-    if((scanf("%d %d", &rows, &cols) == 2)){
+    if(!(scanf("%d %d\n", &rows, &cols) == 2)){
         fprintf(stderr, "Could not read data.");
     }
-    
+    Cell** matrix = (Cell**)create_matrix(rows, cols);
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            scanf("%c", &matrix[i][j].status);
+        }
+        getc(stdin);
+    }
+    print_matrix(matrix, rows, cols);
+    delete_matrix(matrix, rows);
 }
--- refuges.c
+++ refuges.c
@@ -4,5 +4,25 @@
 #include "refuges.h"
 
 Cell** create_matrix(int rows, int cols){
-    
+    Cell** matrix = malloc(rows * sizeof(Cell*));
+    for(int i = 0; i < rows; i++){
+        matrix[i] = malloc(cols * sizeof(Cell));
+    }
+    return matrix;
+}
+
+void delete_matrix(Cell** matrix, int rows){
+    for(int i = 0; i < rows; i++){
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+void print_matrix(Cell** matrix, int rows, int cols){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            putchar(matrix[i][j].status);
+        }
+        putchar('\n');
+    }
 }
