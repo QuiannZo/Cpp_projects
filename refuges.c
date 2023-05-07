5f53152b13b7f36eabafca46c66e85ee11c62d92
Aletu <alejandro.solorzanobaudrit@ucr.ac.cr>
2023-05-06 22:51:11 -0600

    functions were missing from header file, minor changes

--- refuges.c
+++ refuges.c
@@ -3,31 +3,30 @@
 
 #include "refuges.h"
 
-Cell** create_matrix(int rows, int cols){
-    Cell** matrix = malloc(rows * sizeof(Cell*));
-    for(int i = 0; i < rows; i++){
-        matrix[i] = malloc(cols * sizeof(Cell));
+cell_t** create_matrix(int rows, int cols){
+    cell_t** matrix = malloc(rows * sizeof(cell_t*));
+    for(int i = 0; i < rows; i++) {
+        matrix[i] = malloc(cols * sizeof(cell_t));
     }
     return matrix;
 }
 
-void delete_matrix(Cell** matrix, int rows){
-    for(int i = 0; i < rows; i++){
+void delete_matrix(cell_t** matrix, int rows) {
+    for(int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
     free(matrix);
 }
 
-void print_matrix(Cell** matrix, int rows, int cols){
+void print_matrix(cell_t** matrix, int rows, int cols) {
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
             putchar(matrix[i][j].status);
         }
         putchar('\n');
+    }
+}
 
-// Uses "depth-first search" recursive algorithm to visit and mark corresponding cells of a zone as checked if not already
-// This is so that when find_max_size runs, it doesn't call this function on already checked zones.
-// Keeps track of the size of the zone and returns it
 int zone_checker(cell_t** matrix, int row, int col, int rows, int cols, char status) {
     if (row < 0 || row >= rows || col < 0 || col >= cols) {
         return 0;
@@ -44,9 +43,6 @@ int zone_checker(cell_t** matrix, int row, int col, int rows, int cols, char sta
     return zone_size;
 }
 
-// Loops through the area to find '-' that isn't checked, if found then uses zone_checker
-// Uses zone_checker's return value to compare to the biggest size found
-// Returns the biggest zone size found
 int find_max_size(cell_t** matrix, int rows, int cols, char status, int* safe_zone_count) {
     int biggest_size = 0;
     for (int row = 0; row < rows; row++) {
@@ -62,8 +58,6 @@ int find_max_size(cell_t** matrix, int rows, int cols, char status, int* safe_zo
     return biggest_size;
 }  
 
-// Sets every checked bool in the cells in area to false
-// This is to be able to use zone_checker again on change_status()
 void reset_checked(cell_t** matrix, int rows, int cols) {
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; cols++) {
@@ -72,14 +66,7 @@ void reset_checked(cell_t** matrix, int rows, int cols) {
     }
 }
 
-// Changes the status of a specific cell
 void change_status(cell_t** matrix, int rows, int cols, int biggest_size, char status, char new_status) {
-    // reset_checked(rows, cols);
-    // loop through rows and cols of area
-    //     if area[row][col].status = status
-    //         int size = zone_checker(row, col);
-    //         if (size == biggest_size)
-    //             area[row][col].status = new_status;
     reset_checked(matrix, rows, cols);
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
