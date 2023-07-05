dcc0ce91588c48ca9304b29c6ce8155c71f12b60
Quiann <quiann.zolfaghari@ucr.ac.cr>
2023-07-05 16:26:13 -0600

    Modified the RBTree class to allow copies using constructor and =operator

--- src/RBTree/RBTree.hxx
+++ src/RBTree/RBTree.hxx
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <iostream>
 
+/*
 #define DECLARE_RULE5(Class, Action) \
     Class(const Class& other) = Action; \
     Class(Class&& other) = Action; \
@@ -12,12 +13,15 @@
 #define DISABLE_COPY(Class) \
     DECLARE_RULE5(Class, delete);
 
+#define ENABLE_COPY(Class) \
+    DECLARE_RULE5(Class, default);*/
+
+// El constructor por default de las copias no sirve porque se requiere copiar los campos, entonces se hizo manual.
+// No pude hacerlo con macros. 
+
 namespace ecci {
 
-    enum class Color {
-        Red,
-        Black
-    };
+    enum class Color { Red, Black };
 
     template <typename KeyType, typename ValueType, typename Comparator = std::less<KeyType>>
     class RedBlackTree {
@@ -30,16 +34,48 @@ namespace ecci {
             KeyType key;
             ValueType value;
 
-            explicit Node(const KeyType& key, const ValueType& value, Node* parent = nullptr,
-                          Node* left = nullptr, Node* right = nullptr, Color color = Color::Red)
-                : parent(parent),
-                  left(left),
-                  right(right),
-                  color(color),
-                  key(key),
-                  value(value) {}
+            explicit Node(const KeyType& key, const ValueType& value, Node* parent = nullptr, Node* left = nullptr, Node* right = nullptr, Color color = Color::Red)
+            : parent(parent), left(left), right(right), color(color), key(key), value(value) 
+            {}
 
-            DECLARE_RULE5(Node, delete);
+            //DECLARE_RULE5(Iterator, default);
+
+            Node(const Node& other) : parent(nullptr), left(nullptr), right(nullptr), color(other.color), key(other.key), value(other.value) {
+                if (other.left)
+                    left = new Node(*other.left);
+                if (other.right)
+                    right = new Node(*other.right);
+                if (left)
+                    left->parent = this;
+                if (right)
+                    right->parent = this;
+            }
+
+            Node& operator=(const Node& other) {
+                if (this != &other) {
+                    color = other.color;
+                    key = other.key;
+                    value = other.value;
+
+                    delete left;
+                    delete right;
+
+                    left = nullptr;
+                    right = nullptr;
+
+                    if (other.left)
+                        left = new Node(*other.left);
+                    if (other.right)
+                        right = new Node(*other.right);
+
+                    if (left)
+                        left->parent = this;
+                    if (right)
+                        right->parent = this;
+                }
+                return *this;
+            }
+
             ~Node() {
                 delete left;
                 delete right;
@@ -51,6 +87,23 @@ namespace ecci {
 
     public:
         RedBlackTree() : root(nullptr) {}
+
+        RedBlackTree(const RedBlackTree& other) : root(nullptr) {
+            if (other.root)
+                root = new Node(*other.root);
+        }
+
+        RedBlackTree& operator=(const RedBlackTree& other) {
+            if (this != &other) {
+                delete root;
+                root = nullptr;
+
+                if (other.root)
+                    root = new Node(*other.root);
+            }
+            return *this;
+        }
+
         ~RedBlackTree() { delete root; }
 
         class Iterator {
@@ -59,7 +112,8 @@ namespace ecci {
 
         public:
             explicit Iterator(Node* node) : node(node) {}
-            DECLARE_RULE5(Iterator, default);
+
+            //DECLARE_RULE5(Iterator, default);
 
             inline bool operator!=(const Iterator& other) const {
                 return this->node != other.node;
@@ -105,8 +159,13 @@ namespace ecci {
         }
 
         ValueType& operator[](const KeyType& key) {
-            Node* node = insertImpl(key);
-            return node->value;
+            Node* node = findNode(key);
+            if (node) {
+                return node->value;
+            } else {
+                insert(key);
+                return findNode(key)->value;
+            }
         }
 
         void printInOrder() const {
@@ -114,82 +173,125 @@ namespace ecci {
         }
 
     private:
-        Node* insertImpl(const KeyType& key, const ValueType& value = ValueType()) {
-            if (root == nullptr) {
-                root = createNode(key, value, nullptr, nullptr, nullptr, Color::Black);
-                return root;
-            } else {
-                Comparator comparator;
-                Node* current = root;
-                Node* parent = nullptr;
+        // Helper functions
 
-                while (current != nullptr) {
-                    parent = current;
+        Node* findNode(const KeyType& key) const {
+            Node* node = root;
+            Comparator compare;
 
-                    if (comparator(key, current->key))
-                        current = current->left;
-                    else if (comparator(current->key, key))
-                        current = current->right;
-                    else
-                        return current;  // Node with the same key already exists
+            while (node) {
+                if (compare(key, node->key)) {
+                    node = node->left;
+                } else if (compare(node->key, key)) {
+                    node = node->right;
+                } else {
+                    return node;
                 }
-
-                Node* newNode = createNode(key, value, parent, nullptr, nullptr, Color::Red);
-                if (comparator(key, parent->key))
-                    parent->left = newNode;
-                else
-                    parent->right = newNode;
-
-                return newNode;
             }
+
+            return nullptr;
         }
 
-        Node* createNode(const KeyType& key, const ValueType& value, Node* parent, Node* left, Node* right,
-                         Color color) {
-            Node* newNode = new Node(key, value, parent, left, right, color);
-            if (newNode == nullptr)
-                throw std::runtime_error("RedBlackTree: No memory to create node");
+        Node* findMinimum(Node* node) const {
+            if (node) {
+                while (node->left) {
+                    node = node->left;
+                }
+            }
+
+            return node;
+        }
+
+        Node* findMaximum(Node* node) const {
+            if (node) {
+                while (node->right) {
+                    node = node->right;
+                }
+            }
+
+            return node;
+        }
+
+        Node* findNextNode(Node* node) const {
+            if (node) {
+                if (node->right) {
+                    return findMinimum(node->right);
+                } else {
+                    Node* parent = node->parent;
+                    while (parent && node == parent->right) {
+                        node = parent;
+                        parent = parent->parent;
+                    }
+                    return parent;
+                }
+            }
+
+            return nullptr;
+        }
+
+        Node* insertImpl(const KeyType& key, const ValueType& value) {
+            Node* parent = nullptr;
+            Node* node = root;
+            Comparator compare;
+
+            while (node) {
+                parent = node;
+                if (compare(key, node->key)) {
+                    node = node->left;
+                } else if (compare(node->key, key)) {
+                    node = node->right;
+                } else {
+                    // The key already exists, so update the value.
+                    node->value = value;
+                    return node;
+                }
+            }
+
+            Node* newNode = new Node(key, value, parent);
+            if (!parent) {
+                root = newNode;
+            } else if (compare(key, parent->key)) {
+                parent->left = newNode;
+            } else {
+                parent->right = newNode;
+            }
 
             return newNode;
         }
 
-        void fixInsertion(Node* newNode) {
-            while (newNode->parent && newNode->parent->color == Color::Red) {
-                if (newNode->parent == newNode->parent->parent->left) {
-                    Node* uncle = newNode->parent->parent->right;
-
+        void fixInsertion(Node* node) {
+            while (node && node != root && node->parent->color == Color::Red) {
+                if (node->parent == node->parent->parent->left) {
+                    Node* uncle = node->parent->parent->right;
                     if (uncle && uncle->color == Color::Red) {
-                        newNode->parent->color = Color::Black;
+                        node->parent->color = Color::Black;
                         uncle->color = Color::Black;
-                        newNode->parent->parent->color = Color::Red;
-                        newNode = newNode->parent->parent;
+                        node->parent->parent->color = Color::Red;
+                        node = node->parent->parent;
                     } else {
-                        if (newNode == newNode->parent->right) {
-                            newNode = newNode->parent;
-                            rotateLeft(newNode);
+                        if (node == node->parent->right) {
+                            node = node->parent;
+                            rotateLeft(node);
                         }
-
-                        newNode->parent->color = Color::Black;
-                        newNode->parent->parent->color = Color::Red;
-                        rotateRight(newNode->parent->parent);
+                        node->parent->color = Color::Black;
+                        node->parent->parent->color = Color::Red;
+                        rotateRight(node->parent->parent);
                     }
                 } else {
-                    Node* uncle = newNode->parent->parent->left;
-
+                    Node* uncle = node->parent->parent->left;
                     if (uncle && uncle->color == Color::Red) {
-                        newNode->parent->color = Color::Black;
+                        node->parent->color = Color::Black;
                         uncle->color = Color::Black;
-                        newNode->parent->parent->color = Color::Red;
-                        newNode = newNode->parent->parent;
+                        node->parent->parent->color = Color::Red;
+                        node = node->parent->parent;
                     } else {
-                        if (newNode == newNode->parent->left) {
-                            newNode = newNode->parent;
-                            rotateRight(newNode);
+                        if (node == node->parent->left) {
+                            node = node->parent;
+                            rotateRight(node);
                         }
-
-                        newNode->parent->color = Color::Black;
-                        newNode->parent->parent->color = Color::Red;
-                        rotateLeft(newNode->parent->parent);
+                        node->parent->color = Color::Black;
+                        node->parent->parent->color = Color::Red;
+                        rotateLeft(node->parent->parent);
                     }
                 }
             }
@@ -198,76 +300,55 @@ namespace ecci {
         }
 
         void rotateLeft(Node* node) {
+            if (!node || !node->right) {
+                return;
+            }
+
             Node* pivot = node->right;
             node->right = pivot->left;
-
-            if (pivot->left != nullptr)
+            if (pivot->left) {
                 pivot->left->parent = node;
-
+            }
             pivot->parent = node->parent;
-
-            if (node->parent == nullptr)
+            if (!node->parent) {
                 root = pivot;
-            else if (node == node->parent->left)
+            } else if (node == node->parent->left) {
                 node->parent->left = pivot;
-            else
+            } else {
                 node->parent->right = pivot;
-
+            }
             pivot->left = node;
             node->parent = pivot;
         }
 
         void rotateRight(Node* node) {
+            if (!node || !node->left) {
+                return;
+            }
+
             Node* pivot = node->left;
             node->left = pivot->right;
-
-            if (pivot->right != nullptr)
+            if (pivot->right) {
                 pivot->right->parent = node;
-
+            }
             pivot->parent = node->parent;
-
-            if (node->parent == nullptr)
+            if (!node->parent) {
                 root = pivot;
-            else if (node == node->parent->right)
-                node->parent->right = pivot;
-            else
+            } else if (node == node->parent->left) {
                 node->parent->left = pivot;
-
+            } else {
+                node->parent->right = pivot;
+            }
             pivot->right = node;
             node->parent = pivot;
         }
 
-        Node* findMinimum(Node* subtree) {
-            if (subtree == nullptr)
-                return nullptr;
-
-            while (subtree->left != nullptr)
-                subtree = subtree->left;
-
-            return subtree;
-        }
-
-        Node* findNextNode(Node* current) {
-            if (current->right != nullptr)
-                return findMinimum(current->right);
-
-            Node* parent = current->parent;
-
-            while (parent != nullptr && current == parent->right) {
-                current = parent;
-                parent = parent->parent;
-            }
-
-            return parent;
-        }
-
         void printInOrder(Node* node) const {
-            if (node == nullptr)
-                return;
-
-            printInOrder(node->left);
-            std::cout << node->key << ": " << node->value << std::endl;
-            printInOrder(node->right);
+            if (node) {
+                printInOrder(node->left);
+                std::cout << node->key << ": " << node->value << std::endl;
+                printInOrder(node->right);
+            }
         }
     };
 }
