1871e3ad627dec7fdcfabbcec41f91374e4071d7
Quiann <quiann.zolfaghari@ucr.ac.cr>
2023-07-06 23:33:38 -0600

    Modified the Garage.hpp class to store the data necessary for the program.

--- src/Garage.hpp
+++ src/Garage.hpp
@@ -1,17 +1,36 @@
 #pragma once
 
+#include <vector>
+
 #include  "RBTree/RBTree.hxx"
+#include "Driver.hpp"
 
 using ecci::RedBlackTree;
 
 class Garage {
   public:
-    RedBlackTree<int, std::string> kartsAceleration;
-    RedBlackTree<double, std::string> kartsSpeed;
-    RedBlackTree<int, std::string> bikes;
-    RedBlackTree<int, std::string> atvs;
-    RedBlackTree<int, std::string> tires;
+    // Parts by atributes.
+    RedBlackTree<int, std::string> KartsAceleration;
+    RedBlackTree<double, std::string> KartsSpeed;
+    RedBlackTree<int, std::string> BikesAceleration;
+    RedBlackTree<double, std::string> BikesSpeed;
+    RedBlackTree<int, std::string> ATVsAceleration;
+    RedBlackTree<double, std::string> ATVsSpeed;
+    RedBlackTree<int, std::string> TiresTSpeed;
+    RedBlackTree<int, std::string> TiresWSpeed;
+    RedBlackTree<int, std::string> TiresASpeed;
     RedBlackTree<int, std::string> gliders;
+
+    // Tracks by Terrain, water, and air meters.
+    RedBlackTree<int, std::string> TrackT;
+    RedBlackTree<int, std::string> TrackW;
+    RedBlackTree<int, std::string> TrackA;
+
+    // Players.
+    // // When creating the driver sons to store here, the code must get the type of kart from the file input to create
+    // // and store the object on the list.
+    std::vector<Driver> DriverList;
+
   public:
     Garage();
 };
