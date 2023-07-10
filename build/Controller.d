9f4cf79fb06609b16fd95569287f755575aa70b2
Quiann <quiann.zolfaghari@ucr.ac.cr>
2023-07-09 23:57:15 -0600

    Minor changes.

--- build/Controller.d
+++
@@ -1,2 +0,0 @@
-build/Controller.o: src/Controller.cpp src/Controller.hpp src/Garage.hpp \
- src/RBTree/RBTree.hxx src/Driver.hpp
--- build/Garage.d
+++
@@ -1,2 +0,0 @@
-build/Garage.o: src/Garage.cpp src/Garage.hpp src/RBTree/RBTree.hxx \
- src/Driver.hpp
